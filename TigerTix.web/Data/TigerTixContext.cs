3bc5a3805d39edb708c1cc1a8398c062f15eef49
DanteStewart118 <99420498+DanteStewart118@users.noreply.github.com>
2022-03-16 17:34:34 -0400

    * Startup.cs: * TigerTixContext.cs:
    * TigerTix.web.csproj: hate it

--- TigerTix.web/Data/TigerTixContext.cs
+++ TigerTix.web/Data/TigerTixContext.cs
@@ -2,11 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TigerTix.web.Data.Entities;
 
 namespace TigerTix.web.Data
 {
     public class TigerTixContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
 
+        private readonly IConfiguration _config;
+
+        public TigerTixContext (IConfiguration config)
+        {
+            _config = config;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_config["ConnectionStrings:DefaultConnection"]);
+        }
     }
+
+    
 }
--- TigerTix.web/Startup.cs
+++ TigerTix.web/Startup.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TigerTix.web.Data;
 
 namespace TigerTix.web
 {
@@ -16,19 +18,19 @@ namespace TigerTix.web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
-             services.AddControllersWithViews();
+            services.AddDbContext<TigerTixContext> (cfg => {cfg.UseSqlServer(); } );
+            services.AddControllersWithViews();
             
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-          /*  if(env.IsDevelopment())
+            if(env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-          */
+          
              app.UseStaticFiles();
              app.UseRouting();
 
